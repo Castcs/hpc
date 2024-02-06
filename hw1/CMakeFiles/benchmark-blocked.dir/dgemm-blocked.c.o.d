79ab6a68e04c95e5c8a1482f6e995c53ab4f84b5
Cameron Stiver <cstiver@br011.ib.bridges2.psc.edu>
2024-02-05 22:35:39 -0500

    other directory

---
+++ hw1/CMakeFiles/benchmark-blocked.dir/dgemm-blocked.c.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/benchmark-blocked.dir/dgemm-blocked.c.o: \
+ /jet/home/cstiver/hw1/csci4110/hw1/dgemm-blocked.c \
+ /usr/include/stdc-predef.h
---
+++ hw1/CMakeFiles/benchmark-naive.dir/dgemm-naive.c.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/benchmark-naive.dir/dgemm-naive.c.o: \
+ /jet/home/cstiver/hw1/csci4110/hw1/dgemm-naive.c \
+ /usr/include/stdc-predef.h
